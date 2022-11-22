75354f0ffde5da8bcd05547c3abaccb731479a6d
Moisés Masis <moisesalex132@gmail.com>
2022-11-22 16:15:03 -0700

    ADD: password generator

---
+++ generate-password.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"code.google.com/p/go.crypto/bcrypt"
+)
+ 
+func main() {
+	password := flag.String("password", "", "the password to hash or validate")
+
+	hash := flag.String("hash", "", "the hash to validate the password against")
+	
+	cost := flag.Int("cost", 13, "the cost of the bcrypt function to execute")
+ 
+	flag.Parse()
+ 
+	if *hash != "" {
+
+		err := bcrypt.CompareHashAndPassword([]byte(*hash), []byte(*password))
+		if err != nil {
+			fmt.Print("Error: Password and hash do not match.")
+			os.Exit(1)
+		}
+		fmt.Print("OK")
+		os.Exit(0)
+
+	} else {
+
+		generated_hash, err := bcrypt.GenerateFromPassword([]byte(*password), *cost)
+		if err != nil {
+			fmt.Print("Error: Couldn't generate hash.")
+			os.Exit(1)
+		}
+ 
+		fmt.Printf("%s\n", generated_hash)
+		os.Exit(0)
+	}
+}
